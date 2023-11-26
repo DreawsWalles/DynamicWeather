a8aa6d26ce54a45233ccedf739832828d01ff017
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2023-11-27 02:48:05 +0300

    Создание сущностей

---
+++ Business/Entities/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Node
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public double RelativeHumidity { get; set; }
+        public double DewPoint { get; set; } 
+        public int Pressure { get; set; }
+        public required WindDirection Direction { get; set; }
+        public double WildSpeed { get; set; }
+        public double CloudCover { get; set; }
+        public double LowerBoundCloudCover { get; set; }
+        public double HorizontalVisibility { get; set; }    
+
+        public required Document Document { get; set; } 
+    }
+}
---
+++ Business/Entities/WindDirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class WindDirection
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+
+        public virtual ICollection<WindDirection>? Parent { get; set; }
+    }
+}
