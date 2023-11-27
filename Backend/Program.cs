dd5b117f837c4baf9d56483af98f464d12b40205
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2023-11-27 19:24:16 +0300

    Добалвение scoped объектов по типу IRepository, Repository

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,7 @@
+using Business.Repositories.DataRepositories;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Data;
+using Repositories.Repositories.DataRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,11 @@ builder.Services.AddDbContext<Context>(
     optionsLifetime: ServiceLifetime.Transient);
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<INodeRepository, NodeRepository>();
+builder.Services.AddScoped<IWeatherEffectsRepository, WeatherEffectRepository>();
+builder.Services.AddScoped<IWildDirectionRepository, WildDirectionRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
