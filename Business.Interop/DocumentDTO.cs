aa8df26543f2dac0398d6bce1dea1ce59a49f985
Dreaws Walles <85035654+DreawsWalles@users.noreply.github.com>
2023-11-27 20:33:54 +0300

    Исправлегние ошибок в DockerCompose

---
+++ Business.Interop/DocumentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interop
+{
+    public class DocumentDTO
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required ICollection<NodeDTO> Nodes { get; set; }
+    }
+}
---
+++ Business.Interop/NodeDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interop
+{
+    public class NodeDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public double RelativeHumidity { get; set; }
+        public double DewPoint { get; set; }
+        public int Pressure { get; set; }
+        public required ICollection<WildDirectionDTO> Directions { get; set; }
+        public double WildSpeed { get; set; }
+        public double CloudCover { get; set; }
+        public double LowerBoundCloudCover { get; set; }
+        public double HorizontalVisibility { get; set; }
+        public required ICollection<WeatherEffectDTO> WeatherEffects { get; set; }
+
+    }
+}
